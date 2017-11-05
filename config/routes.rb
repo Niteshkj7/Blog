bb1227c069617f719ed64718930654e8ff7ddede
7 <me.nkj7@gmail.com>
2017-11-05 17:40:15 +0530

    few changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  
   get 'welcome/index'
 
   resources :articles do
     resources :comments
   end
   
-  root 'welcome#index'
+  root 'application#hello'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
